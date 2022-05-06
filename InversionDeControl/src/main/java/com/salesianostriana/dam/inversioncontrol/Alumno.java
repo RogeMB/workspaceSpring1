cd77ce1f3dd3e1ed59bc2929225a7387ecae956a
Rogelio Mohigefer Barrera <rogelio.mb@hotmail.com>
2022-05-06 10:53:24 +0200

    changes

---
+++ InversionDeControl/src/main/java/com/salesianostriana/dam/inversioncontrol/Alumno.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Alumno {
+	
+	private Long id;
+	
+	private String nombre;
+	private String apellidos;
+	private String email;
+	
+	private Curso curso;
+	
+	private LocalDate fechaNacimiento;
+
+}
